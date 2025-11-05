87fc4b7d26e58e3d0e6a5f5f16e32c5f06cabc4f
murkaren133 <murkaren133@gmail.com>
2025-11-05 14:55:39 -0500

    callback con manejo de errores

---
+++ callback02.js
@@ -0,0 +1,25 @@
+//Definir una funación de busqueda
+let getUsuarioById = (id, callback) => {
+    //Suponer que realiza una busqueda en base de Datos
+    let usuario = {
+        id, // == id : id
+        nombre: "Valeria"
+    };
+    //Simular un error
+    if (id === 20){
+        //En el caso de error solo se invoca al call back con el mensaje de error
+        callback(`El usuario con id ${id} no existe`)
+    }else{
+        //El primer parámetro del callback es el error, si todo esta correcto, se indica un null
+        callback(null, usuario);
+    }
+};
+
+// utilizar la funcion 
+getUsuarioById(10, (err, usuario) => {
+    if (err){
+        return console.log(err);
+    }
+
+    console.log(usuario);
+});
