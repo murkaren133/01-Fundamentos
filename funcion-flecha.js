b98f66600bf7d0a15e20d88b77a1c76bad18ecf4
murkaren133 <murkaren133@gmail.com>
2025-11-05 15:14:27 -0500

    Funcion callback

--- funcion-flecha.js
+++ funcion-flecha.js
@@ -1,28 +1,35 @@
-// funcion habitual
-/*function sumar (a, b){
+// Función habitual
+function sumarHabitual(a, b){
     return a + b;
-}*/
+}
 
-//declarar la funcion a una constante
-/*const sumar = function(a, b){
+// Declarar la función a una constante
+const sumarConstante = function(a, b){
     return a + b;
-}*/
+}
 
-//funcion flecha 
-/*const sumar = (a, b) => {
+// Función flecha 
+const sumarFlecha = (a, b) => {
     return a + b;
-}*/
+}
 
-//cuando dentro del cuerpo solo existe una instrccion
+// Cuando dentro del cuerpo solo existe una instrucción
 const sumar = (a, b) => a + b;
 
-//Ejercicio
-/*const saludar  = (nombre) =>{
-    return  `hola ${nombre}`
-    //que salude al nombre que reciba
-}*/
+// Ejercicio de saludar
+const saludar1 = (nombre) => {
+    return `Hola ${nombre}`;
+    // que salude al nombre que reciba
+}
 
-/*console.log (saludar("Cristhian"))*/
+// Otra versión simplificada
+const saludar = (nombre) => `Hola ${nombre}`;
 
-const saludar = (nombre) => `hola ${nombre}`;
-console.log (saludar("Valeria"))
+// Pruebas
+console.log(sumarHabitual(2, 3)); // 5
+console.log(sumarConstante(4, 6)); // 10
+console.log(sumarFlecha(1, 9));    // 10
+console.log(sumar(7, 8));          // 15
+
+console.log(saludar1("Cristhian")); // Hola Cristhian
+console.log(saludar("risthian"));    // Hola Valeria
