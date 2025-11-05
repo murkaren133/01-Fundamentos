62ca4cd1843efb3c2c11a0ef804d89a4b1185894
murkaren133 <murkaren133@gmail.com>
2025-11-05 15:13:17 -0500

    Funcion callback

--- callback.js
+++ callback.js
@@ -1,7 +1,8 @@
-/*setTimeout(function(){
-    console.log("Hola!");
-} ,2000);*/
+// Ejemplo 1: Usando una función tradicional con setTimeout
+setTimeout(function() {
+  console.log("Hola!");
+}, 2000);
 
-/*Misma Funcion con la funcion flecha
-let saluda = () => console.log ("Hola");
-setTimeout(saluda , 3000);*/
+// Ejemplo 2: Misma función, pero con una función flecha
+const saludar = () => console.log("Hola!");
+setTimeout(saludar, 3000);
